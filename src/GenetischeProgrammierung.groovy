f719660aa837a011f29e4983af4a9cf1882a2037
Oksana <oksana.semeshkina@stud.hs-mannheim.de>
2016-11-30 20:45:02 +0100

    neue vm
    neue vm

--- src/GenetischeProgrammierung.groovy
+++ src/GenetischeProgrammierung.groovy
@@ -3,21 +3,13 @@
  */
 class GenetischeProgrammierung {
     public static void main(String[] args) {
-        //VM vm = new VM()
-        //vm.simulate()
-        //println "Fitness: ${vm.calculateFitness()}"
-        //vm.anzahlBefehle()
-
         ArrayList<VM> population = new ArrayList<>()
         100.times {
             VM vm = new VM()
             population.add(vm)
-
         }
 
-        //schleife
-        1000.times {
-
+        100000.times {
             population.each {
                 it.reset()
                 it.simulate()
@@ -26,13 +18,13 @@ class GenetischeProgrammierung {
             population.sort(new Comparator<VM>() {
                 @Override
                 int compare(VM o1, VM o2) {
-                    o2.calculateFitness() - o1.calculateFitness()
+                    o2.primeNumbers.size() - o1.primeNumbers.size()
                 }
             })
 
             println("\n" + "-" * 20 + "population" + "-" * 20)
             population.each {
-                print "${it.calculateFitness()} "
+                print "${it.primeNumbers.size()} "
             }
 
             //copy first half of the population into the second half
@@ -43,25 +35,23 @@ class GenetischeProgrammierung {
             }
             population = population + population2
 
-            println("\n" + "-" * 20 + "1/2 population + 1/2 population" + "-" * 20)
+            /*println("\n" + "-" * 20 + "1/2 population + 1/2 population" + "-" * 20)
             population.each {
-                print "${it.calculateFitness()} "
-            }
+                print "${it.primeNumbers.size()} "
+            }*/
 
             //mutate second half
             def mutatePopulation = population.subList(50, 100)
             mutatePopulation.each {
-                it.mutate(10)
+                it.mutate(30)
             }
         }
 
         println "\nPrimzahlen des Besten:"
-        population[0].stack.each {
-            if (VM.istPrimzahl(it))
-                print "$it "
+        population[0].primeNumbers.each {
+            print "$it "
         }
 
         println "\nENDE"
-
     }
 }
--- src/VM.groovy
+++ src/VM.groovy
@@ -19,24 +19,27 @@ public enum befehl {
 }
 
 public class VM {
-    final int MAX = 1000
-    final int MAX_RUNS = 5000
-    final int MEM_WERTE = 10 << 3
+    static final int MAX = 1000
+    static final int MEM_WERTE = 10 << 3
+    static final int MIN_SIZE_OF_THE_PRIME = 0
+    static final int MAX_OPERATIONS_PER_VM_SIMULATION = 1000
 
     ArrayList<Integer> memory = new ArrayList<>()
     ArrayList<Integer> stack = new ArrayList<>()
+    ArrayList<Integer> primeNumbers = new ArrayList<>()
     int programCounter, stackPointer, register
     Random random = new Random()
 
     public VM() {
+        reset()
         fillMemoryRandom()
     }
 
     void push(int x) {
-        if (stackPointer < 999)
-            stack[stackPointer++] = x
-        else
-            stack[stackPointer] = x
+        if (stackPointer >= 0 && stackPointer < MAX) {
+            stack[stackPointer++] = x;
+            addIfPrimeToPrimeNumbers(x)
+        }
     }
 
     int pop() {
@@ -45,50 +48,67 @@ public class VM {
         return stack[stackPointer]
     }
 
-    void simulate() {
-        int i = 0
-        while (programCounter < MAX && (stackPointer > 0 || i < 10) && i < MAX_RUNS) {
-            switch (memory[programCounter] & 7) {
-                case befehl.LOAD.wert:
-                    register = memory[programCounter] >> 3
-                    programCounter++
-                    break
-                case befehl.PUSH.wert:
-                    push(register)
-                    programCounter++
-                    break
-                case befehl.POP.wert:
-                    register = pop()
-                    programCounter++
-                    break
-                case befehl.MUL.wert:
-                    register = register * pop()
-                    programCounter++
-                    break
-                case befehl.DIV.wert:
-                    def wert = pop()
-                    if (wert != 0) {
-                        register = register / wert
-                    }
-                    programCounter++
-                    break
-                case befehl.ADD.wert:
-                    register = register + pop()
-                    programCounter++
-                    break
-                case befehl.SUB.wert:
-                    register = register - pop()
-                    programCounter++
-                    break
-                case befehl.JIH.wert:
-                    if (register > 0) {
-                        programCounter = Math.abs((programCounter + pop())) % MAX
-                    } else {
+    public void simulate() {
+        int pop
+        int counter = 0;
+        while (programCounter < MAX && programCounter >= 0 && counter < MAX_OPERATIONS_PER_VM_SIMULATION && stackPointer >= 0) {
+            counter++;
+            try {
+                switch (memory[programCounter] & 7) {
+                    case befehl.LOAD.wert:
+                        register = memory[programCounter] >> 3;
                         programCounter++
-                    }
-                    break
+                        break
+                    case befehl.PUSH.wert:
+                        push(register);
+                        programCounter++
+                        break
+                    case befehl.POP.wert:
+                        register = this.pop()
+                        programCounter++
+                        break
+                    case befehl.MUL.wert:
+                        pop = this.pop();
+                        register = register * pop;
+                        push(register);
+                        programCounter++
+                        break
+                    case befehl.DIV.wert:
+                        pop = this.pop();
+                        if (pop != 0) {
+                            register = register / pop;
+                        }
+                        push(register);
+                        programCounter++
+                        break
+                    case befehl.ADD.wert:
+                        pop = this.pop();
+                        register = register + pop;
+                        push(register);
+                        programCounter++
+                        break
+                    case befehl.SUB.wert:
+                        pop = this.pop();
+                        register = register - pop;
+                        push(register);
+                        programCounter++
+                        break
+                    case befehl.JIH.wert:
+                        if (register > 0) {
+                            pop = this.pop();
+                            if (pop != 0 && ((programCounter + pop) > 0)) {
+                                programCounter = ((programCounter + pop) % MAX);
+                            }
+                            programCounter++;
+                        } else {
+                            programCounter++;
+                        }
+                        break
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Something is wrong - check your vm");
             }
-            i++
         }
     }
 
@@ -96,17 +116,17 @@ public class VM {
         int zahl
 
         //3 Randomzahlen ins Memory schreiben mit PUSH-Befehl
-        100.times {
-            zahl = random.nextInt(MEM_WERTE)
-            zahl = (zahl & (Integer.MAX_VALUE - 7)) + (int) befehl.LOAD.wert
-            //          println zahl & 7
-            memory.add(zahl)
+        /* 100.times {
+             zahl = random.nextInt(MEM_WERTE)
+             zahl = (zahl & (Integer.MAX_VALUE - 7)) + (int) befehl.LOAD.wert
+             //          println zahl & 7
+             memory.add(zahl)
 
-            zahl = random.nextInt(MEM_WERTE)
-            zahl = (zahl & (Integer.MAX_VALUE - 7)) + (int) befehl.PUSH.wert
-            //            println zahl & 7
-            memory.add(zahl)
-        }
+             zahl = random.nextInt(MEM_WERTE)
+             zahl = (zahl & (Integer.MAX_VALUE - 7)) + (int) befehl.PUSH.wert
+             //            println zahl & 7
+             memory.add(zahl)
+         }*/
 
         //Rest bis 1000 mit beliebigen Befehlen auffüllen
         while (memory.size() < 1000) {
@@ -115,33 +135,36 @@ public class VM {
         }
     }
 
-    int calculateFitness() {
-        HashSet<Integer> prims = new HashSet<>()
-        //    println "Stackgröße: ${stack.size()}"
-        stack.each {
-            if (istPrimzahl(it)) {
-                if (prims.add(it)) {
-                    //  print "$it "
+    private void addIfPrimeToPrimeNumbers(Integer elem) {
+        // avoid negative numbers
+        Integer elemAbs = Math.abs(elem);
+        // only prime numbers bigger than the defined minSizeOfThePrime will be added
+        if (elem > MIN_SIZE_OF_THE_PRIME) {
+            // check if elem is prime
+            if (isPrime(elemAbs)) {
+                // add only new prime numbers
+                if (!primeNumbers.contains(elemAbs)) {
+                    // new prime number --> add
+                    primeNumbers.add(elemAbs);
+                } else {
+                    // prime is known in primeNumbers
+                    // nothing to do
                 }
             }
         }
-        //println()
-        prims.size()
     }
 
-    static boolean istPrimzahl(int zahl) {
-        if (zahl > 1) {
-            boolean isPrimzahl = true;
-            for (int i = 2; i < zahl && isPrimzahl; i++) {
-                if ((zahl % i) == 0) {
-                    isPrimzahl = false;
-                }
-            }
-            return isPrimzahl
-        } else {
-            return false
+    private boolean isPrime(Integer elem) {
+        //check if elem is a multiple of 2
+        if (elem % 2 == 0) return false;
+        //if not, then just check the odds
+        for (int i = 3; i * i <= elem; i += 2) {
+            if (elem % i == 0)
+                return false;
         }
+        return true;
     }
+
 
     def mutate(int amount) {
         amount.times {
@@ -199,13 +222,15 @@ public class VM {
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        VM vm = new VM();
-        vm.stack = this.stack.clone()
+        VM vm = new VM()
+        vm.primeNumbers = this.primeNumbers.clone()
         vm.memory = this.memory.clone()
         return vm
     }
 
     def reset() {
+        this.stack = new int[MAX];
+        this.primeNumbers = new ArrayList<Integer>();
         this.programCounter = 0
         this.stackPointer = 0
         this.register = 0
